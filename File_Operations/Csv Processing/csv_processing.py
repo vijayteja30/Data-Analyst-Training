2738b1ee11a486d1809dd406f847f88b2fd73b7f
vijayteja30 <vijayteja30@gmail.com>
2025-12-11 08:31:31 +0530

    changes added which are staled from long in local

--- File_Operations/Csv Processing/csv_processing.py
+++ File_Operations/Csv Processing/csv_processing.py
@@ -26,11 +26,12 @@ import os
 
 
 import csv
+
 file_path = os.path.join(os.getcwd(), 'File_Operations/Csv Processing/employee_file2.csv')
 with open(file_path, mode='w') as csv_file:
     fieldnames = ['emp_name', 'dept', 'birth_month']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
+    
     writer.writeheader()
     writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
     writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
